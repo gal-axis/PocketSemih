f818f82563ce7c74f6cb3b0db29977007055dff6
goerk <goerk@LAPTOP-CD5NLNHK>
2023-02-08 00:12:46 +0100

    some small changes in game

--- core/src/com/bikinger/semih/Cardinal.java
+++ core/src/com/bikinger/semih/Cardinal.java
@@ -1,6 +1,8 @@
 package com.bikinger.semih;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,23 +10,27 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class Cardinal extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+	public void render() {
+		ScreenUtils.clear(1.5f, 1.0f, 0.9f, 1f);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		boolean keyPressed = Gdx.input.isKeyPressed(Keys.BACKSPACE);
+		if (keyPressed)
+			batch.draw(img, 0, 0);
+
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 		img.dispose();
 	}
